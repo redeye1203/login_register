a1b2f9eb33302fbf3fe86ee756cafc3001670ade
redeye1203 <141793945+redeye1203@users.noreply.github.com>
2024-02-07 00:03:52 +0800

    Delete WebA directory

--- WebA/pay/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include,re_path
-from . import views
-urlpatterns = [
-    re_path(r'^$',views.LoginView.as_view(),name='index'),
-    re_path(r'^transfer/$',views.TransferView.as_view(),name='transfer'),
-]
