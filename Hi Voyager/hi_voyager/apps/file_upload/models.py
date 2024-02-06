0f96c238b20bfa37d5df2f8ec632fadec147ec09
Foylaou <s225002731@gmail.com>
2024-02-07 00:09:16 +0800

    fix

--- Hi Voyager/hi_voyager/apps/file_upload/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class UploadedFile(models.Model):
-    title = models.CharField(max_length=200)
-    uploadedFile = models.FileField(upload_to="Uploaded Files/")
-    dateTimeOfUpload = models.DateTimeField(auto_now=True)
